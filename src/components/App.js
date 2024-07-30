53ce01d17933a42e6b38057e6b6fad8b6593bc99
Caroline <bancroftwanjiru10000@gmail.com>
2024-07-30 15:25:55 +0300

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { format } from "date-fns";
+// react and date-fns are both npm packages.App.js is pulling on specific cnontent foem these two packages.
+// react and date-fns are being imported from node-modules folder which was created when we ran npm install.
 import ExampleComponent from "./ExampleComponent";
-
+// the import for ExampleComponent is slightly different as it is not being imported from a packagehere we are importing something compeletely different
+// from within our project's file tree.
+// by importing ./ExampleCompnent, we make <ExampleComponent /> avaiable for use in the App components return statement.
+import TestComponent from "./TestComponent";
 // Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      <h1>Now</h1> 
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
@@ -14,8 +19,19 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
+      <TestComponent />
     </div>
   );
+  // from the app function that format from the date-fns library is being used in the return statement
+  // when we call format
 }
 
 export default App;
+// with export by including the export line, we are allowing other files to import things from the for example this App.js
+// you can have only one default export per file.
+// there are different tupes of exports, named exports and default exports.
+// Default exports- the line export defalut app denotes that our app function is the main thing we want to export from out app.js file
+// you can have only one default export per file.
+// when you look at index.js file, you can see tat he top of the file that we are importingapp form the app.js file in the src/components direnctory
+// the (.js) deos not need to be included . this import statement is what we use to import somthing that is the default export of another file.
+// import App from "./components/App";
--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+// render method comes from the react Dom package. it takes two arguments: 1. a areact component to render (typically we'll render our top
+// level App component here), 2. A dom element where we want that component to be rendered (by convection, a div with the ID of root)
+// this fucntion s hwow the rest of the of our application will make onto the DOM.
